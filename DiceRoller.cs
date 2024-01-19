d5cd15c85847133a19dfcbc0b2464b027c04dbcb
gardanier22 <agardanier22@gmail.com>
2024-01-18 17:08:29 -0700

    Add project files.

---
+++ DiceRoller.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Mission2Assignment
+{
+    internal class DiceRoller
+    {
+        public static int[] RollSimulator(int numRolls)
+        {
+            // Array with 13 positions to account for the max number of 2 dice (12) even with the poistions of 0 and 1 being n/a
+            int[] results = new int[13];
+
+            //Instance of the class random
+            Random rand = new Random();
+
+            //create a for loop that selects a random number for each die
+            for (int i = 0; i < numRolls; i++)
+            {
+                int dice1 = rand.Next(1, 7);
+                int dice2 = rand.Next(1, 7);
+                int sum = dice1 + dice2;
+
+                results[sum]++;
+            }
+            return results;
+
+        }
+
+    } 
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using Mission2Assignment;
+using System.ComponentModel.Design;
+
+internal class DiceSimulator
+{
+    private static void Main(string[] args)
+    {
+        // Welcome message to the dice throwing simulator and how many times they wanna roll the dice
+        System.Console.WriteLine("Welcome message to the dice throwing simulator!");
+        System.Console.WriteLine("How many dice rolls would you like to take?");
+
+        // Positive integer check
+        string number = System.Console.ReadLine();
+        if (int.TryParse(number, out int numberOfRolls) && numberOfRolls > 0)
+        {
+            int[] myResults = DiceRoller.RollSimulator(numberOfRolls);
+
+            System.Console.WriteLine("\nDice Roll Results");
+            System.Console.WriteLine("Total number of rolls: " + numberOfRolls);
+
+            //start at 2 because we have two dice and the lowest number we can have between the 2 dice is 2
+            // for loop cycles through positions in the array to see how many times each combination occured
+            for (int i = 2; i <= 12; i++)
+            {
+                int percentage = myResults[i] * 100 / numberOfRolls;
+                System.Console.WriteLine(i + ": " + new string('*', percentage));
+            }
+
+            System.Console.WriteLine("\nThanks for playing!");
+            
+        }
+        else
+        {
+            // in the case they enter in not a valid number
+            System.Console.WriteLine("Invalid Input! Please enter a valid input.");
+        }
+    }
+}
